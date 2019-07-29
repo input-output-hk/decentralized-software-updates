bbda544d6ba500d2c3483b8ac7efd60e631eca5f
Damian Nadales <damian.nadales@iohk.io>
2019-07-29 11:36:53 +0200

    Remove windows compilation targets.

--- release.nix
+++ release.nix
@@ -20,11 +20,10 @@ let
   testsSupportedSystems = [ "x86_64-linux" ];
   collectTests = ds: filter (d: elem d.system testsSupportedSystems) (collect isDerivation ds);
 
-  inherit (systems.examples) mingwW64 musl64;
+  inherit (systems.examples) musl64;
 
   jobs = {
     native = mapTestOn (packagePlatforms project);
-    "${mingwW64.config}" = mapTestOnCross mingwW64 (packagePlatformsCross project);
   }
   // {
     # This aggregate job is what IOHK Hydra uses to update
