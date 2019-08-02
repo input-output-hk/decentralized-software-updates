79deaeaf303e076b6fc6e44361718a20b093507d
Damian Nadales <damian.nadales@iohk.io>
2019-08-02 14:24:21 +0200

    Add the notion of block size limits.
    [skip ci]

--- executable-spec/src/Cardano/Ledger/Spec/STS/Chain/Chain.hs
+++ executable-spec/src/Cardano/Ledger/Spec/STS/Chain/Chain.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -6,12 +7,16 @@
 -- ticks.
 module Cardano.Ledger.Spec.STS.Chain.Chain where
 
+import           Data.Word (Word64)
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
+import           Cardano.Prelude (HeapWords, heapWords, heapWords2)
+
 import           Control.State.Transition (Environment, IRC (IRC),
                      PredicateFailure, STS, Signal, State, TRC (TRC),
-                     initialRules, judgmentContext, transitionRules, (?!))
+                     Threshold (Threshold), initialRules, judgmentContext,
+                     transitionRules, (?!))
 import           Control.State.Transition.Generator (HasTrace, envGen, sigGen)
 
 import           Ledger.Core (Slot (Slot))
@@ -28,12 +33,31 @@ data St
 
 data Env
   = Env
-    { initialSlot :: !Slot }
+    { initialSlot :: !Slot
+    , maximumBlockSize :: WordCount
+    -- ^ Maximum block size. For now we measure this in number of 'Word's using
+    -- 'heapWords' from 'Cardano.Prelude.HeapWords'.
+    }
   deriving (Eq, Show)
+
+newtype WordCount = WordCount Word64
+  deriving (Eq, Num, Ord, Show)
 
 data Block
   = Block
-    { slot :: !Slot }
+    { slot :: !Slot
+    , transactions :: [Transaction]
+    }
+    deriving (Eq, Show)
+
+instance HeapWords Block where
+  heapWords (Block (Slot s) transactions) = heapWords2 s transactions
+
+data Transaction = Transaction
+  deriving (Eq, Show)
+
+instance HeapWords Transaction where
+  heapWords Transaction = 0
 
 instance STS CHAIN where
 
@@ -45,6 +69,7 @@ instance STS CHAIN where
 
   data PredicateFailure CHAIN
     = BlockSlotNotIncreasing CurrentSlot Slot
+    | MaximumBlockSizeExceeded WordCount (Threshold WordCount)
     deriving (Eq, Show)
 
 
@@ -57,26 +82,38 @@ instance STS CHAIN where
 
   transitionRules = [
     do
-      TRC (_, St { currentSlot }, Block { slot }) <- judgmentContext
+      TRC ( Env { maximumBlockSize }
+          , St { currentSlot }
+          , block@Block{ slot }) <- judgmentContext
       currentSlot < slot
         ?! BlockSlotNotIncreasing (CurrentSlot currentSlot) slot
+      blockSize block < maximumBlockSize
+        ?! MaximumBlockSizeExceeded (blockSize block) (Threshold maximumBlockSize)
       pure $! St { currentSlot = slot }
     ]
+
+blockSize :: Block -> WordCount
+blockSize = WordCount . fromIntegral . heapWords
 
 -- | Type wrapper that gives more information about what the 'Slot' represents.
 newtype CurrentSlot = CurrentSlot Slot deriving (Eq, Show)
 
 instance HasTrace CHAIN where
 
-  envGen _traceLength = Env <$> gCurrentSlot
+  envGen _traceLength = Env <$> gCurrentSlot <*> gMaxBlockSize
     where
       gCurrentSlot = Slot <$> Gen.integral (Range.constant 0 100)
+      -- For now we fix the maximum block size to 1 KiB.
+      gMaxBlockSize = pure 1024
 
 
   sigGen _ _ St { currentSlot } =
-    Block <$> gNextSlot
+    Block <$> gNextSlot <*> gTransactions
     where
       -- We'd expect the slot increment to be close to 1, even for large Gen's
       -- size numbers.
       gNextSlot =  Slot . (s +) <$> Gen.integral (Range.exponential 1 10)
-      Slot s = currentSlot
+        where
+          Slot s = currentSlot
+
+      gTransactions = pure []
