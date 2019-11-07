a4007eb1a277758dc64c9a7bd8526364128c254b
Damian Nadales <damian.nadales@iohk.io>
2019-11-07 16:52:48 +0100

    Use `iohk-nix-utils` for building on Buildkite (#67)

--- nix/.stack.nix/default.nix
+++ nix/.stack.nix/default.nix
@@ -15,7 +15,6 @@
         "bimap" = (((hackage.bimap)."0.4.0").revisions)."d56712c094d40b4666e30d35fb0dad4058a2b753c5af4574f062237b2f7d5b67";
         "generic-monoid" = (((hackage.generic-monoid)."0.1.0.0").revisions)."00d1a7b7ff0890a3e3cba4032be24fe377c26e5e45d73afcacd42c98abb3b7e3";
         "terminal-progress-bar" = (((hackage.terminal-progress-bar)."0.4.1").revisions)."ba857f3424ddb1034125163a9a384e9baab22e55de968259b046892c20ec0526";
-        } // {
         decentralized-updates = ./decentralized-updates.nix;
         cardano-prelude = ./cardano-prelude.nix;
         cardano-binary = ./cardano-binary.nix;
@@ -29,5 +28,6 @@
       compiler.nix-name = "ghc865";
       };
   resolver = "lts-13.26";
+  modules = [ ({ lib, ... }: { packages = {}; }) { packages = {}; } ];
   compiler = "ghc-8.6.5";
   }
