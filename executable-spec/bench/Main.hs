cdb41b874bfd441ddb499d9aaacd89020bcc8295
nkarag <nkarag@gmail.com>
2020-02-11 17:07:36 +0200

    small correction

---
+++ executable-spec/bench/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import           Cardano.Ledger.Benchmarks.Update.WorstCaseAnalysis
+import 			 Cardano.Ledger.MBenchmarks.Update.Tally.TallyMicroBenchmark
+
+main :: IO ()
+main = do
+  putStrLn worstCaseAnalysisTable
+  putStrLn worstCaseAnalysisPlot
---
+++ executable-spec/bench/micro-benchmarking/Cardano/Ledger/MBenchmarks/Update/Tally/TallyMicroBenchmark.hs
@@ -0,0 +1,5 @@
+module Cardano.Ledger.MBenchmarks.Update.Tally.TallyMicroBenchmark where
+
+import 		Cardano.Ledger.Spec.State.ProposalsState (tally)
+
+f = undefined
