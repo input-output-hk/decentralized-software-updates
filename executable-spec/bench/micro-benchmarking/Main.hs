f5f10d252d3710c3f73205bd5b26069e0fd648ac
nkarag <nkarag@gmail.com>
2020-03-06 16:46:31 +0200

    Added elapsed time and memory consumption measurements

--- executable-spec/bench/micro-benchmarking/Main.hs
+++ executable-spec/bench/micro-benchmarking/Main.hs
@@ -12,28 +12,28 @@ import qualified Criterion.Main as Cr
 main :: IO ()
 main = do
   let
-      -- !tallyData2 =
-      --   createTallyData constants (NumberOfParticipants 10)      (NumberOfConcurrentUPs 10)
-        -- !tallyData3 =
-        --  createTallyData constants (NumberOfParticipants 100)     (NumberOfConcurrentUPs 10)
-      -- !tallyData4 =
-      --   createTallyData constants (NumberOfParticipants 1000)    (NumberOfConcurrentUPs 10)
-      -- !tallyData5 =
-      --   createTallyData constants (NumberOfParticipants 10000)   (NumberOfConcurrentUPs 10)
-      -- !tallyData6 =
-      --   createTallyData constants (NumberOfParticipants 100000)  (NumberOfConcurrentUPs 10)
+      !tallyData2 =
+        createTallyData constants (NumberOfParticipants 100)      (NumberOfConcurrentUPs 1)
+      !tallyData3 =
+         createTallyData constants (NumberOfParticipants 1000)     (NumberOfConcurrentUPs 1)
+      !tallyData4 =
+        createTallyData constants (NumberOfParticipants 10000)    (NumberOfConcurrentUPs 1)
+      !tallyData5 =
+        createTallyData constants (NumberOfParticipants 100000)   (NumberOfConcurrentUPs 1)
+      !tallyData6 =
+        createTallyData constants (NumberOfParticipants 1000000)  (NumberOfConcurrentUPs 1)
       !tallyData7 =
-        createTallyData constants (NumberOfParticipants 1000000) (NumberOfConcurrentUPs 10)
-  print $ runTally constants tallyData7
-  -- Cr.defaultMain
-  --   [ Cr.bgroup "tally" [ Cr.bench "1e2" $ Cr.whnf allApproved tallyData2
-  --                       , Cr.bench "1e3" $ Cr.whnf allApproved tallyData3
-  --                       , Cr.bench "1e4" $ Cr.whnf allApproved tallyData4
-  --                       , Cr.bench "1e5" $ Cr.whnf allApproved tallyData5
-  --                       , Cr.bench "1e6" $ Cr.whnf allApproved tallyData6
-  --                       , Cr.bench "1e7" $ Cr.whnf allApproved tallyData7
-  --                       ]
-  --   ]
+        createTallyData constants (NumberOfParticipants 10000000) (NumberOfConcurrentUPs 1)
+  print $ runTally constants tallyData2
+  Cr.defaultMain
+    [ Cr.bgroup "tally" [ Cr.bench "1e2" $ Cr.whnf allApproved tallyData2
+                        , Cr.bench "1e3" $ Cr.whnf allApproved tallyData3
+                        , Cr.bench "1e4" $ Cr.whnf allApproved tallyData4
+                        , Cr.bench "1e5" $ Cr.whnf allApproved tallyData5
+                        , Cr.bench "1e6" $ Cr.whnf allApproved tallyData6
+                        , Cr.bench "1e7" $ Cr.whnf allApproved tallyData7
+                        ]
+    ]
   where
     !constants =
       BenchmarkConstants
