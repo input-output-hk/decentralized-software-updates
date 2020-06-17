8445d28a10dbd8bf8557f64f567b2b02db33f58f
Craige McWhirter <craige.mcwhirter@iohk.io>
2020-06-17 13:04:30 +1000

    Switched from ghc.baseGhc to merely ghc

--- nix/stack-shell.nix
+++ nix/stack-shell.nix
@@ -3,15 +3,13 @@
 with import ./. {};
 
 haskell.lib.buildStackProject {
-  name = "stack-env";
-  ghc = (import ../shell.nix { inherit pkgs; }).ghc.baseGhc;
+  name = "decentralised-software-updates-stack-env";
+  ghc = (import ../shell.nix { inherit pkgs; }).ghc;
 
   buildInputs =
     # Development libraries which may be necessary for the build.
     # TODO: Add remove libraries as necessary
     [ zlib gmp ncurses lzma openssl git systemd.dev ] ++
-    # MacOS-specific librararies which may be necessary for the build.
-    (lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ Cocoa CoreServices libcxx libiconv ])) ++
     # Add any programs/tools which are required for e.g. running tests.
     (with haskellNixPackages; [ /* TODO: Put programs here */ ]);
 
